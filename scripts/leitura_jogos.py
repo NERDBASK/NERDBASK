9dfea20e92bfa42d7fb7a63db5764efb50e8a8ae
IgorNascAlves <igor.nascimento.flipe@gmail.com>
2021-07-28 23:44:33 -0300

    unificação dos csv's de todos os períodos

--- scripts/leitura_jogos.py
+++ scripts/leitura_jogos.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import requests
-import json
 import sys
 import os
 
@@ -22,49 +21,73 @@ def retorna_header():
 
   return headers
 
-def jogo_por_Q(GameID, startPeriod, endPeriod):
-  
-  num_Q = f'Q{startPeriod}' if startPeriod == endPeriod else f'T{startPeriod}-{endPeriod}'
+def jogo_todos_q(GameID):
 
-  rangeType = '1'
-  startRange = '0'
-  endRange = '31800'
+	status_code = 200
+	df_players_all = pd.DataFrame()
+	df_game_statistics_all = pd.DataFrame()
+	Q = 1
+	nome_pasta_jogo, gameId = '', ''
+	rangeType = '1'
+	startRange = '0'
+	endRange = '31800'
 
-  headers = retorna_header()  
-  url_base = f'https://stats.nba.com/stats/boxscoretraditionalv3?GameID={GameID}&LeagueID=00&'
-  url_stats = f'{url_base}endPeriod={endPeriod}&endRange={endRange}&rangeType={rangeType}&startPeriod={startPeriod}&startRange={startRange}'
+	headers = retorna_header()  
+	url_base = f'https://stats.nba.com/stats/boxscoretraditionalv3?GameID={GameID}&LeagueID=00&'
 
-  with requests.Session() as sessao:
-          resposta: requests.models.Response = sessao.get(url_stats, headers=headers)
+	while status_code != None:
 
-  y = json.loads(resposta.content)
+		startPeriod, endPeriod = Q, Q
+		
+		url_stats = f'{url_base}endPeriod={endPeriod}&endRange={endRange}&rangeType={rangeType}&startPeriod={startPeriod}&startRange={startRange}'
 
-  name_homeTeam = y['boxScoreTraditional']['homeTeam']['teamName']
-  name_awayTeam = y['boxScoreTraditional']['awayTeam']['teamName']
-  gameId = y['boxScoreTraditional']['gameId']
-  
-  nome_pasta_jogo = f'{gameId}_{name_homeTeam}_{name_awayTeam}'
-  
-  try:
-    os.mkdir(f'dados/{nome_pasta_jogo}')
-  except OSError:
-    pass
+		with requests.Session() as sessao:
+				resposta: requests.models.Response = sessao.get(url_stats, headers=headers)
 
+		y = resposta.json()
+		
+		status_code = y['boxScoreTraditional']['homeTeam']['teamName']
 
-  df_players_homeTeam = pd.json_normalize(y['boxScoreTraditional']['homeTeam']['players'])
-  df_players_homeTeam['teamName'] = name_homeTeam
-  df_players_awayTeam = pd.json_normalize(y['boxScoreTraditional']['awayTeam']['players'])
-  df_players_awayTeam['teamName'] = name_awayTeam
+		if status_code != None:
 
-  df_players = df_players_awayTeam.append(df_players_homeTeam)
-  df_players.to_csv(f'dados/{nome_pasta_jogo}/{gameId}_players_{num_Q}.csv', index=False)
+			name_homeTeam = y['boxScoreTraditional']['homeTeam']['teamName']
+			name_awayTeam = y['boxScoreTraditional']['awayTeam']['teamName']
+			gameId = y['boxScoreTraditional']['gameId']
+			
+			nome_pasta_jogo = f'{gameId}_{name_homeTeam}_{name_awayTeam}'
 
-  df_awayTeam_statistics = pd.json_normalize(y['boxScoreTraditional']['awayTeam']['statistics'])
-  df_homeTeam_statistics = pd.json_normalize(y['boxScoreTraditional']['homeTeam']['statistics'])
-  df_game_statistics = df_homeTeam_statistics.append(df_awayTeam_statistics)
-  df_game_statistics.index = [name_homeTeam, name_awayTeam]
-  df_game_statistics.to_csv(f'dados/{nome_pasta_jogo}/{gameId}_game_statistics_{num_Q}.csv', index=False)
+			df_players_homeTeam = pd.json_normalize(y['boxScoreTraditional']['homeTeam']['players'])
+			df_players_homeTeam['teamName'] = name_homeTeam
+			df_players_awayTeam = pd.json_normalize(y['boxScoreTraditional']['awayTeam']['players'])
+			df_players_awayTeam['teamName'] = name_awayTeam
+			df_players = df_players_awayTeam.append(df_players_homeTeam)
 
-_, GameID, startPeriod, endPeriod = sys.argv
+			df_players['Q'] = Q
+			df_players_all = df_players_all.append(df_players)
 
-jogo_por_Q(GameID, startPeriod, endPeriod)
+			df_awayTeam_statistics = pd.json_normalize(y['boxScoreTraditional']['awayTeam']['statistics'])
+			df_awayTeam_statistics['teamName'] = name_awayTeam
+			df_homeTeam_statistics = pd.json_normalize(y['boxScoreTraditional']['homeTeam']['statistics'])
+			df_homeTeam_statistics['teamName'] = name_homeTeam
+			df_game_statistics = df_homeTeam_statistics.append(df_awayTeam_statistics)
+			
+			df_game_statistics['Q'] = Q			
+			df_game_statistics_all = df_game_statistics_all.append(df_game_statistics)
+
+		Q += 1
+
+	try:
+		os.mkdir(f'dados/{nome_pasta_jogo}')
+	except OSError:
+		pass
+
+	df_players_all.to_csv(f'dados/{nome_pasta_jogo}/{gameId}_players_statistics.csv', index=False)
+	df_game_statistics_all.to_csv(f'dados/{nome_pasta_jogo}/{gameId}_team_statistics.csv', index=False)
+
+_, GameID = sys.argv
+
+jogo_todos_q('0042000' + GameID)
+
+#0042000217 NetsXBucks
+#0042000402 SunsXBucks
+#0042000406 BucksXSuns
